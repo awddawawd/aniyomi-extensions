1d093592f30175bf2748d90a2368c71e37d82a4e
awddawawd <57504682+awddawawd@users.noreply.github.com>
2026-07-25 13:46:35 +0000

    fix

--- lib/filemoon-extractor/build.gradle.kts
+++ lib/filemoon-extractor/build.gradle.kts
@@ -1,13 +1,35 @@
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("kotlinx-serialization")
+    kotlin("android")
+    // Corrected the serialization plugin ID for Kotlin Gradle DSL
+    kotlin("plugin.serialization") 
+}
+
+android {
+    namespace = "eu.kanade.tachiyomi.lib.filemoonextractor"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
-    // Core Aniyomi dependencies (provided by the app)
-    compileOnly("eu.kanade.tachiyomi:extensions-lib:14.0.0")
-    compileOnly("eu.kanade.tachiyomi.lib:playlist-utils:1.0.0")
+    // 1. Corrected the version from 14.0.0 to 1.4
+    compileOnly("eu.kanade.tachiyomi:extensions-lib:1.4") 
+
+    // 2. Changed to a local project dependency instead of a remote JitPack fetch
+    implementation(project(":lib:playlist-utils"))
 
     // Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
