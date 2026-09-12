7ad5724f622d4ed4b6d6e08a2eb1ccf0846b8a3c
Nothex <anon>
2026-09-12 21:57:57 +0200

    Ensure apk files are included in repo branch by removing gitignore and using git add -f

--- .github/scripts/commit-repo.sh
+++ .github/scripts/commit-repo.sh
@@ -2,11 +2,12 @@
 set -e
 
 rsync -a --delete --exclude .git --exclude .gitignore ../master/repo/ .
+rm -f .gitignore
 git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
+git add -f .
 git status
 if [ -n "$(git status --porcelain)" ]; then
-    git add .
     git commit -m "Update extensions repo"
     git push origin HEAD:repo
 
